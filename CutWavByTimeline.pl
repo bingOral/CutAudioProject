3d301f8581bcb8d025896815c767450958e5ef5c
Yankaituo <yankaituo@gmail.com>
2019-01-23 11:18:28 +0800

    bugfix

--- CutWavByTimeline.pl
+++ CutWavByTimeline.pl
@@ -3,8 +3,16 @@
 use strict;
 use JSON;
 use Encode;
+use File::Copy;
+
+if(scalar(@ARGV) != 2)
+{
+	print "Usage : perl $0 infile outfile\n";
+	exit;
+}
 
 open(IN,$ARGV[0])||die("The file can't find!\n");
+open(OUT,">$ARGV[1]")||die("The file can't find!\n");
 
 my @task = <IN>;
 dowork(\@task);
@@ -15,13 +23,22 @@ sub dowork
 	foreach my $movie (@$task)
 	{
 		chomp($movie);
-		print $movie."\n";
+
+		print "Before :".$movie."\n";
+		$movie = pro($movie);
+		print "After  :".$movie."\n";
+
 		my $wav = formate($movie);
 		my $srt = getSrt($movie);
-		my $json = getJson($srt);
 		my $dir = getDir($movie);
-		my $res = cut($wav,$dir,$json);
-		die;
+		if($srt)
+		{
+			my $json = getJson($srt);
+			print "wav : ".$wav."\nsrt : ".$srt."\ndir : ".$dir."\njson : ".$json."\n";
+
+			cut($wav,$dir,$json);
+		}
+		#die;
 	}
 }
 
@@ -68,16 +85,34 @@ sub getSrt
 		$srt = $1.'.srt';
 		$dir = $2;
 	}
-	
-	qx(asstosrt -f $ass -o $dir);
-	qx(iconv -f UTF-16LE -t UTF-8 $srt -o $srt);
-	return $srt;
+
+	if( -e $ass)
+	{	
+		qx(asstosrt -f $ass -o $dir);
+		qx(iconv -f UTF-16LE -t UTF-8 $srt -o $srt);
+		return $srt;	
+	}
+	else
+	{
+		$srt = search($movie,$dir);
+		#qx(iconv -f UTF-16LE -t UTF-8 $srt -o $srt);
+
+		if($srt)
+		{
+			return $srt;	
+		}
+		else
+		{
+			return;
+		}
+	}
 }
 
 sub getJson
 {
 	my $srt = shift;
 	my $str = "perl script/srt2json.pl -f $srt -v";
+	print $str."\n";
 	system($str);
 	return $srt.'.json';
 }
@@ -113,8 +148,8 @@ sub cut
 		my $filename = $dir.'/'.$prefix.'-'.($i+1).'.wav';	
         	my $str = "ffmpeg -v quiet -y -i ".$wav." -ss ".$start_time." -to ".$end_time." -acodec copy ".$filename;
 		print $str."\n";
-		system($str);
-		print $filename."|".formater($texts)."\n";
+		system($str) unless -e $filename;
+		print OUT $filename."|".formater($texts)."\n";
 	}	
 }
 
@@ -123,9 +158,53 @@ sub formater
         my $info = shift;
         unless(Encode::is_utf8($info))
         {   
-                $info = Encode::decode('utf8',$info);
+                $info = Encode::decode('iso-8859-1',$info);
         }   
         return $info;
+}
+
+sub search
+{
+	my $movie = shift;
+	my $dir = shift;
+
+	my $str;
+	if($movie =~ /.*\/(.*).mkv/)
+	{
+		$str = $1;
+	}
+
+	opendir(DIR,$dir)||die("Can't open this dir!\n");
+	my @files = readdir DIR;
+	my @files_pro;
+
+	foreach my $file (@files)
+	{
+		if($file =~ /.*.srt$/)
+		{
+			$file = pro($dir.$file);
+			push @files_pro, $file;	
+		}		
+	}
+
+	foreach my $file (@files_pro)
+	{
+		if($file =~ /$str.*.srt$/)
+		{
+			return $file;
+		}
+	}	
+	return;
+}
+
+sub pro 
+{
+	my $filename = shift;
+	my $newname = $filename;
+	$newname =~ s/[^a-zA-Z0-9_\-.\/]//g;
+
+	copy($filename,$newname) unless -e $newname;
+	return $newname;		
 }
 
 1;
