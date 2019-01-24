d919fd11cefe9d15a3f5a468c045f24afc8cbaf5
Yankaituo <yankaituo@gmail.com>
2019-01-24 11:10:19 +0800

    add

---
+++ script/getWavLength.py
@@ -0,0 +1,9 @@
+import wave,sys
+import contextlib
+
+fname = sys.argv[1]
+with contextlib.closing(wave.open(fname,'r')) as f:
+    frames = f.getnframes()
+    rate = f.getframerate()
+    duration = frames / float(rate)
+    print(duration)
